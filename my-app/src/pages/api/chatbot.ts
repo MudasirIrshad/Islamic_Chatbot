5c9c38d27808cd585d163f6fdf36d4d278859f21
MudasirIrshad <mudasirirshad47@gmail.com>
2025-02-11 20:58:50 +0500

    working

---
+++ my-app/src/pages/api/chatbot.ts
@@ -0,0 +1,34 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import Groq from "groq-sdk";
+// Initialize OpenAI API client
+const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
+
+// pages/api/chatbot.ts
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    const { message } = req.body;
+    if (!message) {
+      return res.status(400).send({ error: "Message parameter is required" });
+    }
+
+    const response = await groq.chat.completions.create({
+      model: "llama-3.3-70b-versatile",
+      messages: [
+        {
+          role: "system",
+          content:
+            "You are an AI trained in Islamic knowledge. Only provide answers based on the Quran, Hadith, and scholarly interpretations. Avoid personal opinions.",
+        },
+        { role: "user", content: message as string },
+      ],
+    });
+
+    res.status(200).send({ answer: response.choices[0].message?.content });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ error: "Failed to fetch response" });
+  }
+}
