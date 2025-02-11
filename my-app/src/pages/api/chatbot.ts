8ca48e6bd3ada6af064566c232d66a8ece2d4949
MudasirIrshad <mudasirirshad47@gmail.com>
2025-02-11 21:38:16 +0500

    working

--- my-app/src/pages/api/chatbot.ts
+++ my-app/src/pages/api/chatbot.ts
@@ -8,27 +8,34 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  try {
-    const { message } = req.body;
-    if (!message) {
-      return res.status(400).send({ error: "Message parameter is required" });
-    }
-
-    const response = await groq.chat.completions.create({
-      model: "llama-3.3-70b-versatile",
-      messages: [
-        {
-          role: "system",
-          content:
-            "You are an AI trained in Islamic knowledge. Only provide answers based on the Quran, Hadith, and scholarly interpretations. Avoid personal opinions.",
-        },
-        { role: "user", content: message as string },
-      ],
-    });
-
-    res.status(200).send({ answer: response.choices[0].message?.content });
-  } catch (error) {
-    console.log(error);
-    res.status(500).send({ error: "Failed to fetch response" });
+  if (req.method === "GET") {
+    res.status(200).json({ message: "This is a GET request" });
+  } else if (req.method === "POST") {
+    res.status(200).json({ message: "This is a POST request" });
+  } else {
+    res.status(405).json({ message: "Method Not Allowed" });
   }
+  // try {
+  //   const { message } = req.body;
+  //   if (!message) {
+  //     return res.status(400).send({ error: "Message parameter is required" });
+  //   }
+
+  //   const response = await groq.chat.completions.create({
+  //     model: "llama-3.3-70b-versatile",
+  //     messages: [
+  //       {
+  //         role: "system",
+  //         content:
+  //           "Suppose you are an dr. israr ahmed, molana azad, iqbal, engineer muhammad ali mirza, molana ishaq type islamic schollar and your response should be with references of quran and hadith, but give short and too the point answer if arbi is required give arbi too (ayats).",
+  //       },
+  //       { role: "user", content: message as string },
+  //     ],
+  //   });
+
+  //   res.status(200).send({ answer: response.choices[0].message?.content });
+  // } catch (error) {
+  //   console.log(error);
+  //   res.status(500).send({ error: "Failed to fetch response" });
+  // }
 }
